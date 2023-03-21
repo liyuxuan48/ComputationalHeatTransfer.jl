75de1cfa4c4113af6e8bb2c6bc2abb330a0392ba
Yuxuan Li <liyuxuan48@live.com>
2023-03-20 20:46:18 -0700

    Update fileIO.jl

--- src/fileIO.jl
+++ src/fileIO.jl
@@ -2,9 +2,9 @@ export setup_examples
 
 function setup_examples(workingdir,filetype="all")
     if filetype == "all" || filetype == "notebook" 
-        # if !isdir(workingdir,"examples") 
-        #     mkdir(joinpath(workingdir,"examples"))
-        # end
+        if !isdir(workingdir,"examples") 
+            mkdir(joinpath(workingdir,"examples"))
+        end
 
         exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../examples")
         for (root, dirs, files) in walkdir(exdir)
@@ -20,7 +20,7 @@ function setup_examples(workingdir,filetype="all")
             mkdir(joinpath(workingdir,"expdata"))
         end
 
-        exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../examples/expdata")
+        exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../expdata")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
                 cp(joinpath(root, file),joinpath(workingdir,"expdata",file),force=true)
