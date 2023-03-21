9da470e0996c6ba461c39945c72d7481eb72d936
Yuxuan Li <liyuxuan48@live.com>
2023-03-20 18:49:44 -0700

    Update fileIO.jl

--- src/fileIO.jl
+++ src/fileIO.jl
@@ -2,28 +2,36 @@ export setup_examples
 
 function setup_examples(workingdir,filetype="all")
     if filetype == "all" || filetype == "notebook" 
-        mkdir(joinpath(workingdir,"examples"))
+        if !isdir(workingdir,"examples") 
+            mkdir(joinpath(workingdir,"examples"))
+        end
+
         exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../examples")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
                 cp(joinpath(root, file),joinpath(workingdir,"examples",file),force=true)
-                chmod(joinpath(workingdir,file),0o644)
+                chmod(joinpath(workingdir,"examples",file),0o644)
             end
         end
     end
 
     if filetype == "all" || filetype == "experiment" 
-        mkdir(joinpath(workingdir,"expdata"))
+        if !isdir(workingdir,"expdata") 
+            mkdir(joinpath(workingdir,"expdata"))
+        end
+
         exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../expdata")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
                 cp(joinpath(root, file),joinpath(workingdir,"expdata",file),force=true)
-                chmod(joinpath(workingdir,file),0o644)
+                chmod(joinpath(workingdir,"expdata",file),0o644)
             end
         end
     end    
 
     if filetype == "all" || filetype == "simulation" 
-        mkdir(joinpath(workingdir,"numedata"))
+        if !isdir(workingdir,"numedata") 
+            mkdir(joinpath(workingdir,"numedata"))
+        end
     end    
 end
