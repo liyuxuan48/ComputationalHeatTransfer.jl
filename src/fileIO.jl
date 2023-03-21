0c74d13b15da4426060590a461c2b83f2cd129a5
Yuxuan Li <liyuxuan48@live.com>
2023-03-20 17:11:00 -0700

    Update fileIO.jl

--- src/fileIO.jl
+++ src/fileIO.jl
@@ -5,7 +5,7 @@ function setup_examples(workingdir,filetype="all")
         exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../examples")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
-                cp(joinpath(root, file),joinpath(workingdir,file),force=true)
+                cp(joinpath(root, file),joinpath(workingdir,"examples",file),force=true)
                 chmod(joinpath(workingdir,file),0o644)
             end
         end
@@ -15,9 +15,13 @@ function setup_examples(workingdir,filetype="all")
         exdir = joinpath(dirname(pathof(ComputationalHeatTransfer)),"../expdata")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
-                cp(joinpath(root, file),joinpath(workingdir,file),force=true)
+                cp(joinpath(root, file),joinpath(workingdir,"expdata",file),force=true)
                 chmod(joinpath(workingdir,file),0o644)
             end
         end
+    end    
+
+    if filetype == "all" || filetype == "simulation" 
+        mkdir(joinpath(workingdir,"numedata"),mode = 0o644)
     end    
 end
