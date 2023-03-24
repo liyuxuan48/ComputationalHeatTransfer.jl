dae6e0f3c38dc5e3e63941d3c64b4357c3f494ee
liyuxuan48 <liyuxuan48@live.com>
2023-03-24 13:14:25 -0700

    Update fileIO.jl

--- src/fileIO.jl
+++ src/fileIO.jl
@@ -10,7 +10,7 @@ function setup_examples(workingdir,filetype="all")
         for (root, dirs, files) in walkdir(exdir)
             for file in files
                 cp(joinpath(root, file),joinpath(workingdir,"examples",file),force=true)
-                chmod(joinpath(workingdir,file),0o644)
+                chmod(joinpath(workingdir,"examples",file),0o644)
             end
         end
     end
