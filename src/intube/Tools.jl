8c44d6a8ff7c013a5ef23b7d579003a981da26f8
Yuxuan Li <liyuxuan48@live.com>
2025-09-21 14:17:02 -0400

    Update Tools.jl

--- src/intube/Tools.jl
+++ src/intube/Tools.jl
@@ -720,38 +720,38 @@ function getCa(μ,σ,velocity)
     Ca = abs.(μ.*velocity./σ)
 end
 
-function filmδcorr(Ca,d)
-    filmδ = d .* 0.67.*Ca.^(2/3)./(1 .+ 3.35.*Ca.^(2/3))
-end
+# function filmδcorr(Ca,d)
+#     filmδ = d .* 0.67.*Ca.^(2/3)./(1 .+ 3.35.*Ca.^(2/3))
+# end
 
-function getAdeposit(sys)
-    dXdt= sys.liquid.dXdt
-    Ac= sys.tube.Ac
-    d = sys.tube.d
-    δ = sys.vapor.δ
-    μₗ = sys.liquid.μₗ
-    σ = sys.liquid.σ
+# function getAdeposit(sys)
+#     dXdt= sys.liquid.dXdt
+#     Ac= sys.tube.Ac
+#     d = sys.tube.d
+#     δ = sys.vapor.δ
+#     μₗ = sys.liquid.μₗ
+#     σ = sys.liquid.σ
 
-    numofliquidslug = length(dXdt)
+#     numofliquidslug = length(dXdt)
 
-    δarea = Ac .* (1 .- ((d .- 2*δ ) ./ d) .^ 2);
+#     δarea = Ac .* (1 .- ((d .- 2*δ ) ./ d) .^ 2);
 
-# need to initialize it later on
-    Adeposit = deepcopy(dXdt)
+# # need to initialize it later on
+#     Adeposit = deepcopy(dXdt)
 
-    Ca = getCa.(μₗ,σ,dXdt)
-    δarea_corr = getδarea.(Ac,d,filmδcorr.(Ca,d))
+#     Ca = getCa.(μₗ,σ,dXdt)
+#     δarea_corr = getδarea.(Ac,d,filmδcorr.(Ca,d))
 
 
-    for i = 1:length(Adeposit)
-        loop_index = (i != numofliquidslug) ? i+1 : 1
-        Adeposit_left = dXdt[i][1] > 0 ? δarea_corr[i][1] : δarea[i]
-        Adeposit_right = dXdt[i][end] < 0 ? δarea_corr[i][end] : δarea[loop_index]
-        Adeposit[i]  =   (Adeposit_left, Adeposit_right)
-    end
+#     for i = 1:length(Adeposit)
+#         loop_index = (i != numofliquidslug) ? i+1 : 1
+#         Adeposit_left = dXdt[i][1] > 0 ? δarea_corr[i][1] : δarea[i]
+#         Adeposit_right = dXdt[i][end] < 0 ? δarea_corr[i][end] : δarea[loop_index]
+#         Adeposit[i]  =   (Adeposit_left, Adeposit_right)
+#     end
 
-    Adeposit
-end
+#     Adeposit
+# end
 
 # function getAdeposit(sys,δdeposit)
 #     dXdt= sys.liquid.dXdt
@@ -826,9 +826,21 @@ function f_churchill(Re,ϵ=0.001)
     f
 end
 
+# function Catoδ(d,Ca;adjust_factor=1,δmin=2e-6,δmax=1e-4)
+
+#     δ = Ca .^ (2/3) ./ (1 .+ Ca .^ (2/3)) .* d ./ 2 .* adjust_factor
+#     if (δ < δmin)
+#         return δmin
+#     elseif (δ > δmax)
+#         return δmax
+#     else 
+#         return δ
+#     end
+# end
+
 function Catoδ(d,Ca;adjust_factor=1,δmin=2e-6,δmax=1e-4)
 
-    δ = Ca .^ (2/3) ./ (1 .+ Ca .^ (2/3)) .* d ./ 2 .* adjust_factor
+    δ = 1.34*Ca .^ (2/3) ./ (1 .+ 3.35* Ca .^ (2/3)) .* d ./ 2 .* adjust_factor
     if (δ < δmin)
         return δmin
     elseif (δ > δmax)
