7ac514be1c3a6474172036fb1e34ad1c8dbb3f4e
Yuxuan Li <liyuxuan48@live.com>
2025-11-12 20:57:19 -0600

    build unit tests

--- test/integrator.jl
+++ test/integrator.jl
@@ -447,7 +447,12 @@ end
     @test !all(isapprox.(integrator_tube.p.wall.θarray,Tref, rtol=1e-10))
 
     store!(SimuResult,integrator_tube,integrator_plate)
+    
+    #make sure after time marching, the system state is up-to-date with the latest u
+    p_old = deepcopy(integrator_tube.p)
+    OscillatingHeatPipe.exchangepinfo!(integrator_tube,integrator_plate)
 
+    @test all(p_old.vapor.P .== integrator_tube.p.vapor.P)
     @test SimuResult.tube_hist_θwall[end] == integrator_tube.p.wall.θarray
     @test SimuResult.tube_hist_u[end] == integrator_tube.u
     @test SimuResult.tube_hist_t[end] == integrator_tube.t
